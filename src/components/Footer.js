c66651ce8ce32144478474ee07a40e488747c6fe
Dick Tonye <31224101+tonyedick@users.noreply.github.com>
2021-07-04 00:48:42 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { ChipIcon } from "@heroicons/react/solid";
+import {FaTwitter} from "react-icons/fa";
+import {FaFacebook} from "react-icons/fa";
+import {FaGoogle} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
 
 export default function Footer() {
     return (
@@ -9,10 +13,16 @@ export default function Footer() {
             <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
               Follow me on Social Media
             </h1>
-            <ChipIcon className="w-10 inline-block mb-4" />
-            <ChipIcon className="w-10 inline-block mb-4" />
-            <ChipIcon className="w-10 inline-block mb-4" />
-            <ChipIcon className="w-10 inline-block mb-4" />
+            <a href="https://www.twitter.com/dick_teejay" className="twitter social">
+              <FaTwitter className="w-10 inline-block mb-4" /></a>
+            <a href="https://www.facebook.com/dick.teejay" className="facebook social">
+              <FaFacebook className="w-10 inline-block mb-4" /></a>
+            <a href="https://www.gmail.com/djoeton" className="gmail social">
+              <FaGoogle className="w-10 inline-block mb-4" /></a>
+            <a href="https://www.linkedin.com/in/dicktonye" className="linkedin social">
+              <FaLinkedin className="w-10 inline-block mb-4" /></a>
+            <a href="https://www.github.com/dicktonye" className="github social">
+              <FaGithub className="w-10 inline-block mb-4" /></a>
           </div>
         </div>
        </section>
