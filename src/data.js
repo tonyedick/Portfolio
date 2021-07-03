e7d4bd2ad7eb9205704d57ef7bf1cf0acbe15ca1
tonyedick <djoeton@gmail.com>
2021-07-03 20:33:05 +0200

    Npm install

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const projects = [
+  {
+    title: "PBE Organics",
+    subtitle: "Ecommerce Development",
+    description:
+      "An ecommerce platform that sells organic products and skin care products to customers in Nigeria and abroad.",
+    image: "./project-1.gif",
+    link: "https://pbeorganics.com",
+  },
+  {
+    title: "Abundant Health Specialist Hospital",
+    subtitle: "Web Design",
+    description:
+      "Web Design project for a hospital based in Port Harcourt, Nigeria. I incorporated fully functional SEO for the website.",
+    image: "./project-2.gif",
+    link: "https://abundanthealthspecialist.com",
+  },
+  {
+    title: "Eggheads International School",
+    subtitle: "School Management System",
+    description:
+      "Built a full school management system for a school based in Port Harcourt. Redesigned the school website and added responsive dashboard using Material UI",
+    image: "./project-3.gif",
+    link: "https://eggheadschools.com",
+  },
+  {
+    title: "Epic Track App",
+    subtitle: "React",
+    description:
+      "A simple React app with amazing features like track updates, reactions and login dashboard. Users can create account and upload audio tracks",
+    image: "./project-4.gif",
+    link: "https://linkedin.com/in/dicktonye",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Mr. Dick, Tonye is resilient, passionate and pays attention to details. He followed up with us for more than a year as a prospective client, until we were fully set for our website design. He keeps in contact with us to make sure we are satisfied at each step",
+    image: "/client-2.jpg",
+    name: "Dr. Julius Edeogu",
+    company: "Abundant Health Specialist Hospital",
+  },
+  {
+    quote:
+      "This was the first time I developed a website for my brand, so I couldn’t have asked for a better or easier person to work with. The entire process from design through development and launch was timely and extremely efficient. He renders advice in many areas beyond website development to ensure my brand stands out from the crowd. A fantastic experience all around!",
+    image: "/client-3.jpg",
+    name: "Sandra Okoh",
+    company: "CEO Peace Beauty Empire",
+  },
+  {
+    quote:
+      "I highly recommend Mr. Tonye for the quality of work he delivers. He redesigned the school's website and added new functionalities that we now enjoy.",
+    image: "/client-4.jpg",
+    name: "Beloveth Edem",
+    company: "Administrator - Eggheads International Schools",
+  },
+  {
+    quote:
+      "Tonye helped me bring my website into the 21st century. With his development expertise and the WordPress format, I can now keep my website up-to-date myself without relying on an expensive designer. I immediately saw an improvement in my search engine results and no longer shy away from referring leads to research my site.",
+    image: "/client-5.jpg",
+    name: "Kemebi Timi-Johnson",
+    company: "HSE Officer",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
