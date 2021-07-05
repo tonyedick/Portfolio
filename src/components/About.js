9e967f9eee469dc7a8035201f29d41ee87dcd363
tonyedick <djoeton@gmail.com>
2021-07-05 17:05:30 +0200

    Random changes
    Commit for random changes with profile image

--- src/components/About.js
+++ src/components/About.js
@@ -31,7 +31,7 @@ export default function About() {
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <img
               className="object-cover object-center rounded"
-              alt="hero"
+              alt="mr tonye"
               src="./coding.svg"
             />
           </div>
