1d8e7003704d5cc5013b54643cfc31702d4869dc
Dick Tonye <31224101+tonyedick@users.noreply.github.com>
2022-08-17 09:29:53 +0100

    Reordered projects

--- src/data.js
+++ src/data.js
@@ -1,19 +1,35 @@
 export const projects = [
   {
-    title: "Glistars Store",
-    subtitle: "E-commerce Website Project",
-    description:
-      "An ecommerce platform (built with PHP framework) that connects merchants, store owners, manufacturers of a wide range of products to customers in Nigeria and around Africa.",
-    image: "./project-11.gif",
-    link: "https://glistarsstore.com",
-  },
-  {
     title: "Abundant Health Specialist Hospital",
     subtitle: "Web Design",
     description:
       "Web Design project for a hospital based in Port Harcourt, Nigeria. I incorporated fully functional SEO for the website.",
     image: "./project-2.gif",
     link: "https://abundanthealthspecialist.com",
+  },
+  {
+    title: "Fashion Ecommerce website - Ongoing project",
+    subtitle: "Modest Couture",
+    description:
+      "Modest couture is an Afro infused and urban brand founded in Canada, they deal in t-shirts, hoodies, tank tops, joggers, ready to wear",
+    image: "./project-1.gif",
+    link: "https://linkedin.com/in/dicktonye",
+  },  
+  {
+    title: "Ayaweisoft",
+    subtitle: "Website Redesign",
+    description:
+      "A redesign of Ayaweisoft company website from an old website to a more responsive and interactive website using Bootstrap 5 and SASS",
+    image: "./project-6.gif",
+    link: "https://ayaweisoft.com",
+  },
+  {
+    title: "Glistars Store",
+    subtitle: "E-commerce Website Project",
+    description:
+      "An ecommerce platform (built with PHP framework) that connects merchants, store owners, manufacturers of a wide range of products to customers in Nigeria and around Africa.",
+    image: "./project-11.gif",
+    link: "https://glistarsstore.com",
   },
   {
     title: "Eggheads International School",
@@ -24,30 +40,12 @@ export const projects = [
     link: "https://eggheadsintlschool.com",
   },
   {
-    title: "Fashion Ecommerce website - Ongoing project",
-    subtitle: "Modest Couture",
-    description:
-      "Modest couture is an Afro infused and urban brand founded in Canada, they deal in t-shirts, hoodies, tank tops, joggers, ready to wear",
-    image: "./project-1.gif",
-    link: "https://linkedin.com/in/dicktonye",
-  },
-  
-  {
     title: "Sojilearn - ongoing project",
     subtitle: "Online Tech Training Institute",
     description:
       "Sojilearn is a Technology Workforce Development company that helps people learn premium technology skills virtually and partners with companies to hire the best talents and invest in workforce development. Currently with learners from 19 countries",
     image: "./project-55.gif",
     link: "https://sojilearn.com",
-  },
-  
-  {
-    title: "Ayaweisoft",
-    subtitle: "Website Redesign",
-    description:
-      "A redesign of Ayaweisoft company website from an old website to a more responsive and interactive website using Bootstrap 5 and SASS",
-    image: "./project-6.gif",
-    link: "https://ayaweisoft.com",
   },
 ];
 
@@ -75,7 +73,7 @@ export const testimonials = [
   },
   {
     quote:
-      "Tonye helped me bring my website into the 21st century. With his development expertise and the WordPress format, I can now keep my website up-to-date myself without relying on an expensive designer. I immediately saw an improvement in my search engine results and no longer shy away from referring leads to research my site.",
+      "Mr Tonye helped me bring my website into the 21st century. With his development expertise and the WordPress format, I can now keep my website up-to-date myself without relying on an expensive designer. I immediately saw an improvement in my search engine results and no longer shy away from referring leads to research my site.",
     image: "/client-5.jpg",
     name: "Kemebi Timi-Johnson",
     company: "HSE Officer",
