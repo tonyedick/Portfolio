b5d901029bdfaf67539069de7e1afbf4d26891f8
Dick Tonye <31224101+tonyedick@users.noreply.github.com>
2022-08-17 04:42:39 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,8 @@ export default function About() {
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
               Hi, I'm Dick Tonye.
-              <br className="hidden lg:inline-block" /> I love to build amazing
-              solutions.
+              <br className="hidden lg:inline-block" /> I enjoy building amazing software
+              solutions to help businesses maximize profit..
             </h1>
             <p className="mb-8 leading-relaxed">
             My goal is to help businesses properly communicate their solutions 
