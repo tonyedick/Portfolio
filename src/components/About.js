8c63088496e7a52821aa5c6df15bce1cebdbc7b2
Dick Tonye <31224101+tonyedick@users.noreply.github.com>
2022-10-24 23:33:08 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -15,27 +15,9 @@ export default function About() {
             My goal is to help businesses properly communicate their solutions 
             with minimum resources and time spent.
             </p>
-            <div className="flex justify-center">
-              <a
-                href="#contact"
-                className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                Work With Me
-              </a>
-              <a
-                href="#projects"
-                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                See My Past Work
-              </a>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
-              className="object-cover object-center rounded"
-              alt="mr tonye"
-              src="./profile.png"
-            />
           </div>
         </div>
       </section>
+        
     );
   }
